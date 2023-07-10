bf22e42af824745e0665b11eff9f959ebe174f1f
Trevor Vardeman <trevortx@gmail.com>
2023-07-09 17:05:24 -0700

    added best practice and explanation

--- practices/2023-07-09-moving-zeros-to-the-end.js
+++ practices/2023-07-09-moving-zeros-to-the-end.js
@@ -1,7 +1,7 @@
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
-
+//my first attempt
 function moveZeros(arr) {
   const newArray = []
   let counter = 0
@@ -20,3 +20,11 @@ function moveZeros(arr) {
 }
 
 console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
+
+// best practice
+// var moveZeros = function (arr) {
+//   return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+// }
+
+// explanation
+// this is much more concise. it first filters the array to remove all zeroes, then concats that array with another filtered array that's only returning zeroes. since it's concatendated, they're joined, so the zeroes simply get added on to the end of the concatented array.
