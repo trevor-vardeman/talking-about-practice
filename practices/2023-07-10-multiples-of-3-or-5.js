61f3b4232ed47e6ccede5cfa57970c448f427ccb
Trevor Vardeman <trevortx@gmail.com>
2023-07-10 22:33:43 -0700

    added simplified solution to multiples-of-3-or-5

--- practices/2023-07-10-multiples-of-3-or-5.js
+++ practices/2023-07-10-multiples-of-3-or-5.js
@@ -17,3 +17,19 @@ function solution(number){
 
   solution(10)
   solution(23)
+
+  // better solution
+
+//   function solution(number){
+//     var sum = 0;
+    
+//     for(var i = 1;i< number; i++){
+//       if(i % 3 == 0 || i % 5 == 0){
+//         sum += i
+//       }
+//     }
+//     return sum;
+//   }
+
+// explanation
+// this solution is better because there's no need for an array of numbers that needs to be reduced in my solution, you can simplify by doing it this way.
