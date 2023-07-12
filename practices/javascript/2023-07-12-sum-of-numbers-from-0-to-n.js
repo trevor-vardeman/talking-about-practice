954ade8026ac237556e94a9be0ac805b0a9d61de
Trevor Vardeman <trevortx@gmail.com>
2023-07-12 05:42:31 -0700

    added a new problem

---
+++ practices/javascript/2023-07-12-sum-of-numbers-from-0-to-n.js
@@ -0,0 +1,9 @@
+// Description:
+
+// We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+function sequenceSum(count) {
+    
+  };
+
+  sequenceSum(6)
