7fab05831e9bd020bb1dcd2ad5e56d329247e995
Trevor Vardeman <trevortx@gmail.com>
2023-07-14 17:17:21 -0700

    added my solution, and actually, i like it better than the top one, so i'm not even going to add a 'better' solution

--- practices/javascript/2023-07-12-sum-of-numbers-from-0-to-n.js
+++ practices/javascript/2023-07-12-sum-of-numbers-from-0-to-n.js
@@ -2,8 +2,25 @@
 
 // We want to generate a function that computes the series starting from 0 and ending until the given number.
 
-function sequenceSum(count) {
-    
+var SequenceSum = (function() {
+  function SequenceSum() {}
+
+  SequenceSum.showSequence = function(count) {
+    if (count < 0) return `${count}<0`
+    if (count === 0 ) return "0=0"
+    let sum = 0
+    let output = ""
+    for (let i = 0; i < count + 1; i++) {
+        sum += i
+        if (i === count) {
+          output += i
+        } else {
+          output += `${i}+`
+        }
+    }
+    return `${output} = ${sum}`
   };
 
-  sequenceSum(6)
+  return SequenceSum;
+
+})();
