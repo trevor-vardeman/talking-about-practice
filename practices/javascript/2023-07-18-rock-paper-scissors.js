1d458c311c9ced52178a3881a8f620ab667f386a
Trevor Vardeman <trevortx@gmail.com>
2023-07-18 23:00:09 -0700

    this is the type of solution i was waiting for to create simple rules. really effective, really elegant.

--- practices/javascript/2023-07-18-rock-paper-scissors.js
+++ practices/javascript/2023-07-18-rock-paper-scissors.js
@@ -16,3 +16,21 @@ const rps = (p1, p2) => {
     if (p1 === "scissors" && p2 === "paper") return "Player 1 won!"
     if (p1 === "paper" && p2 === "scissors") return "Player 2 won!"
   };
+
+// better solution
+
+// const rps = (p1, p2) => {
+//     if (p1 === p2) return "Draw!";
+//     var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+//     if (p2 === rules[p1]) {
+//       return "Player 1 won!";
+//     }
+//     else {
+//       return "Player 2 won!";
+//     }
+//   };
+
+// rps("scissors", "rock")
+
+// explanation
+// this is the kind of solution i was looking for -- much more elegant. here, we're creating an object with the rules. the keys are the rps, and the values are the only other ones they beat. then, you create an 'if' statement that checks if the second player's answer matches the value of the first player's selection. if so, player 1 wins, if not, player 2 wins. beautiful.
