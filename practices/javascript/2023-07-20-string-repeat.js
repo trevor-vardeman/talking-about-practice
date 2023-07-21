f127c2227aa4ed59461f2465f78ccfc6c5e01f03
Trevor Vardeman <trevortx@gmail.com>
2023-07-20 18:59:06 -0700

    submitting my result, that was more challenging than expected. wondering what the better solution will look like

--- practices/javascript/2023-07-20-string-repeat.js
+++ practices/javascript/2023-07-20-string-repeat.js
@@ -4,3 +4,10 @@
 // 6, "I"     -> "IIIIII"
 // 5, "Hello" -> "HelloHelloHelloHelloHello"
 
+function repeatStr (n, s) {
+    let newString = ""
+    for (let i = 0; i < n; i++) {
+      newString += s
+    }
+    return newString
+  }
