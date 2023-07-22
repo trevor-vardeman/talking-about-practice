25b15cc688950716c2f00038c480c0c988b57a98
Trevor Vardeman <trevortx@gmail.com>
2023-07-21 22:33:24 -0700

    i got fairly close on this one, but it was tough

--- practices/javascript/2023-07-23-your-order-please.js
+++ practices/javascript/2023-07-23-your-order-please.js
@@ -9,3 +9,9 @@
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 // ""  -->  ""
 
+// function order(words){
+  
+//     return words.split(' ').sort(function(a, b){
+//         return a.match(/\d/) - b.match(/\d/);
+//      }).join(' ');
+//   }    
