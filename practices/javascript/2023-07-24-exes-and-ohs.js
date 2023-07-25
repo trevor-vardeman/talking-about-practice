829a0c536a1e450c37ae732204a9ab18814e8d25
Trevor Vardeman <trevortx@gmail.com>
2023-07-24 22:47:06 -0700

    added the better solution

--- practices/javascript/2023-07-24-exes-and-ohs.js
+++ practices/javascript/2023-07-24-exes-and-ohs.js
@@ -8,24 +8,32 @@
 // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 // XO("zzoo") => false
 
-function XO(str) {
-    const x = () => {
-      if (str.toLowerCase().match(/x/g) === null) {
-        return 0
-      } else {
-        return str.toLowerCase().match(/x/g).length
-      }
-    }
-    const o = () => {
-      if (str.toLowerCase().match(/o/g) === null) {
-        return 0
-      } else {
-        return str.toLowerCase().match(/o/g).length
-      }
-    }
-    if (x() === o()) { 
-      return true
-    } else { 
-      return false
-    }
-}
+// function XO(str) {
+//     const x = () => {
+//       if (str.toLowerCase().match(/x/g) === null) {
+//         return 0
+//       } else {
+//         return str.toLowerCase().match(/x/g).length
+//       }
+//     }
+//     const o = () => {
+//       if (str.toLowerCase().match(/o/g) === null) {
+//         return 0
+//       } else {
+//         return str.toLowerCase().match(/o/g).length
+//       }
+//     }
+//     if (x() === o()) { 
+//       return true
+//     } else { 
+//       return false
+//     }
+// }
+
+// better soltuion
+
+// function XO(str) {
+//     let x = str.match(/x/gi);
+//     let o = str.match(/o/gi);
+//     console.log((x && x.length) === (o && o.length));
+//   }
