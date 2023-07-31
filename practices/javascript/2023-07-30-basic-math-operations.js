8f0dcadf8ea961b0dc7767d7484ff4aa90522032
Trevor Vardeman <trevortx@gmail.com>
2023-07-30 22:22:31 -0700

    added a second solution and explanation

--- practices/javascript/2023-07-30-basic-math-operations.js
+++ practices/javascript/2023-07-30-basic-math-operations.js
@@ -15,3 +15,19 @@ function basicOp(operation, value1, value2) {
     if (operation === "*") return value1 * value2
     if (operation === "/") return value1 / value2
   }
+
+// another solution
+
+// function basicOp(operation, value1, value2) {
+//     switch (operation) {
+//         case '+': return value1 + value2;
+//         case '-': return value1 - value2;
+//         case '*': return value1 * value2;
+//         case '/': return value1 / value2;
+//         default: return 'Operation must be one of + - * /';
+//     }
+// }
+
+// explanation
+
+// i like switch here because it's kind of what switch is made for. if works, but switch also seems elegant to me.
