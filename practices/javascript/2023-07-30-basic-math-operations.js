f4578d6dfb752516d2955cdb897f5ef9a4f92e5e
Trevor Vardeman <trevortx@gmail.com>
2023-07-30 22:20:19 -0700

    adding my solution

--- practices/javascript/2023-07-30-basic-math-operations.js
+++ practices/javascript/2023-07-30-basic-math-operations.js
@@ -9,3 +9,9 @@
 // ('*', 5, 5) --> 25
 // ('/', 49, 7) --> 7
 
+function basicOp(operation, value1, value2) {
+    if (operation === "+") return value1 + value2
+    if (operation === "-") return value1 - value2
+    if (operation === "*") return value1 * value2
+    if (operation === "/") return value1 / value2
+  }
