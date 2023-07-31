e3dc706a230f1e3ab0a77120a605fe9fa7c7b2a6
Trevor Vardeman <trevortx@gmail.com>
2023-07-30 22:44:28 -0700

    added another solution that i like a little more

--- practices/javascript/2023-07-30-calculate-average.js
+++ practices/javascript/2023-07-30-calculate-average.js
@@ -5,4 +5,14 @@
 function findAverage(array) {
     if (array.length === 0) return 0
     return array.reduce((acc, x) => acc += x)/array.length
-  }
+}
+
+// another solution
+
+// const find_average = (array) => {  
+//     return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+// }
+
+// explanation
+
+// i like the simplicity of using the ternary operator instead of a single 'if' statement to account for an empty array. much more elegant!
