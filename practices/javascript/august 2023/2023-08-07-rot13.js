dc1c4393e5011897c2b769c9c876c26aa57bc495
Trevor Vardeman <trevortx@gmail.com>
2023-08-07 22:59:19 -0700

    added my solution

--- practices/javascript/august 2023/2023-08-07-rot13.js
+++ practices/javascript/august 2023/2023-08-07-rot13.js
@@ -12,3 +12,21 @@
 
 // "This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
 
+function rot13(str) {
+    const alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const lowercaseStr = str.toLowerCase()
+    let finalizedRot13 = ""
+    for (let i = 0; i < str.length; i++) {
+      let index = alphabetArray.findIndex(letter => letter === lowercaseStr[i]) + 13
+      const isUppercase = letter => /[A-Z]/.test(letter)
+      if (index === 12) {
+        finalizedRot13 += str.charAt(i)
+      } else if (index > 25) {
+        let newIndex = index - 26
+        isUppercase(str[i]) ? finalizedRot13 += alphabetArray[newIndex].toUpperCase() : finalizedRot13 += alphabetArray[newIndex]
+      } else {
+        isUppercase(str[i]) ? finalizedRot13 += alphabetArray[index].toUpperCase() : finalizedRot13 += alphabetArray[index]
+      }
+    }
+    return finalizedRot13
+  }
