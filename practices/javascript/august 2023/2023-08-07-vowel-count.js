a79b1e56aea62194810f3d0c5eb09cf02aeb401d
Trevor Vardeman <trevortx@gmail.com>
2023-08-07 18:43:12 -0700

    added my solution

--- practices/javascript/august 2023/2023-08-07-vowel-count.js
+++ practices/javascript/august 2023/2023-08-07-vowel-count.js
@@ -4,3 +4,11 @@
 
 // The input string will only consist of lower case letters and/or spaces.
 
+function getCount(str) {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u")
+        count += 1
+    }
+    return count
+  }
