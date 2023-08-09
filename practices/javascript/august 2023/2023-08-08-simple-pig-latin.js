61e9431c089509c02f3d866f59799e29effd2bba
Trevor Vardeman <trevortx@gmail.com>
2023-08-08 20:20:50 -0700

    pig latin solved

--- practices/javascript/august 2023/2023-08-08-simple-pig-latin.js
+++ practices/javascript/august 2023/2023-08-08-simple-pig-latin.js
@@ -4,3 +4,15 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
+function pigIt(str){
+    let regex = /[a-z]/i
+    let rearrangedWord = str.split(" ").map(word => {
+      let newWord = ""
+      let firstLetter = word.substring(0, 1)
+      let coreWord = word.substring(1)
+      word.match(regex) ? newWord += coreWord + firstLetter + "ay" : newWord += word
+      return newWord
+    })
+    return rearrangedWord.join(" ")
+}
+
