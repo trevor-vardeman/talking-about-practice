7bc90b462c65f6382033aa931e27f53fe8e181ee
Trevor Vardeman <trevortx@gmail.com>
2023-08-10 21:50:47 -0700

    added today's first problem

---
+++ practices/javascript/august 2023/2023-08-10-quadrants.js
@@ -0,0 +1,9 @@
+// Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+// Examples
+// (1, 2)     => 1
+// (3, 5)     => 1
+// (-10, 100) => 2
+// (-1, -9)   => 3
+// (19, -56)  => 4
+
