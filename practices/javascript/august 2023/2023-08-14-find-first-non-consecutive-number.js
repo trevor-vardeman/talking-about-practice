4ea907c68291ea7e1018509eb25e125e143b5e82
Trevor Vardeman <trevortx@gmail.com>
2023-08-14 23:40:22 -0700

    removed my console log and moved the 'null' return down a line for clarity

--- practices/javascript/august 2023/2023-08-14-find-first-non-consecutive-number.js
+++ practices/javascript/august 2023/2023-08-14-find-first-non-consecutive-number.js
@@ -22,9 +22,9 @@
 function firstNonConsecutive (arr) {
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i + 1] !== arr[i] + 1) {
-        console.log("arr[i + 1]", arr[i + 1], "arr[i] + 1", arr[i] + 1)
         return arr[i + 1]
       }
-    } return null
+    } 
+    return null
 }
 
